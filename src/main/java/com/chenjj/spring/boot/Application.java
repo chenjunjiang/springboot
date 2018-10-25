78e44c4d1380220624ec05361ca0d93b6987ac8b
chenjunjiang <chenjunjiang1988@126.com>
2018-10-25 21:37:47 +0800

    add ClassLocationUtils

--- src/main/java/com/chenjj/spring/boot/Application.java
+++ src/main/java/com/chenjj/spring/boot/Application.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("Application started");
     }
 
     @RequestMapping("/")
