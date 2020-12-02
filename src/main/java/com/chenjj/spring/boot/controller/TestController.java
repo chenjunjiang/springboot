0d4a3d706a6cc704df57d8392a62df32d6be0abf
陈俊江 <https://gitee.com/chenjunjiang>
2020-12-02 14:08:56 +0800

    使用devtools热部署

--- src/main/java/com/chenjj/spring/boot/controller/TestController.java
+++ src/main/java/com/chenjj/spring/boot/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.chenjj.spring.boot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class TestController {
     @RequestMapping("/mock")
     public String mock(String name) {
         return "Hello" + name + "!";
+    }
+
+    @GetMapping("/hello")
+    public String  hello() {
+        return "Hello DevTools xxx";
     }
 }
